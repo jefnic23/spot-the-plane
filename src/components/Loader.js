6b07e99cb38b424757a0c99379fc0be99e5bea0b
Jeff <jefnic23@gmail.com>
2022-07-18 13:44:38 -0400

    improved loader css

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,10 +1,21 @@
 import styles from '../styles/Loader.module.css';
 
 export default function Loader() {
+    const bubbles = () => {
+        let items = [];
+        for (let i=1; i < 21; i++) {
+            items.push(<span key={i} style={{"--i": i}}></span>);
+        }
+        return items;
+    }
+
     return (
         <div className={styles.container}>
             <span className={styles.title}>Loading Data</span>
-            <span className={styles.loader}></span>
+            <div className={styles.loader}>
+                {bubbles()}
+                <div className={styles.plane}></div>
+            </div>
         </div>
     );
 }
