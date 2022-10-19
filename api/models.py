2b7e6ac2e750583278f03741fe83263860e69a9e
Jeff <jefnic23@gmail.com>
2022-10-18 22:20:13 -0400

    Created new database tables

--- api/models.py
+++ api/models.py
@@ -8,3 +8,10 @@ class Aircraft(db.Model):
     model = db.Column(db.String(), nullable=False)
     typecode = db.Column(db.String(), nullable=False)
     viable = db.Column(db.Boolean, nullable=False, default=True)
+
+
+class PlaneTypes(db.Model):
+    __tablename__ = "planetypes"
+    model = db.Column(db.String(), primary_key=True)
+    num_planes = db.Column(db.Integer, nullable=False)
+    weight = db.Column(db.Float, nullable=True)
--- api/utils/data.py
+++ api/utils/data.py
@@ -1,16 +1,45 @@
+import os
+
 import numpy as np
 import pandas as pd
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from tqdm import tqdm
 
 from typecodes import typecodes
 
-url = 'https://opensky-network.org/datasets/metadata/aircraftDatabase.csv'
-df = pd.read_csv(url, index_col='registration')
+
+def db_engine():
+    load_dotenv()
+    USER = os.getenv('USER')
+    PSWD = os.getenv('PSWD')
+    HOST = os.getenv('HOST')
+    PORT = os.getenv('PORT')
+    NAME = os.getenv('NAME')
+    return create_engine(f'postgresql://{USER}:{PSWD}@{HOST}:{PORT}/{NAME}')
 
 
-for model, typecode in typecodes.items():
-    model = df[df['model'].str.contains(typecode, na=False)][['manufacturericao', 'model', 'typecode']].dropna()
-    # todo: append to local db
-    # model.to_sql()
+def get_aircraft_data():
+    url = 'https://opensky-network.org/datasets/metadata/aircraftDatabase.csv'
+    return pd.read_csv(url, index_col='registration', usecols=['registration', 'manufacturericao', 'model', 'typecode'])
+
+
+def main():
+    engine = db_engine()
+    df = get_aircraft_data()
+
+    for model, typecode in tqdm(typecodes.items()):
+        aircraft = df[df['typecode'].str.contains(typecode, na=False)].dropna()
+        aircraft["typecode"] = model  # override typecode column with model name, to normalize data
+        aircraft.to_sql('aircraft', engine, if_exists='append')
+
+        num_planes = aircraft[aircraft.columns[0]].count()  # supposedly faster than just getting len(index)
+        planetypes = pd.DataFrame({'model': [model], 'num_planes': [num_planes], 'weight': [np.nan]}).set_index('model')
+        planetypes.to_sql('planetypes', engine, if_exists='append')
+
+
+if __name__ == "__main__":
+    main()
 
 
 ##########
