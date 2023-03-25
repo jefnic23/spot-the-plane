47d5bc35fbd7c8f2f217a74e23c9fc9e9f8009b0
Jeff Nicholas <jefnic23@gmail.com>
2023-03-25 19:06:44 -0400

    added fk on models

--- api/models.py
+++ api/models.py
@@ -1,26 +1,30 @@
-from sqlalchemy import Boolean, Column, Float, Integer, String
 from database import Base
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
 
 
 class Aircraft(Base):
     __tablename__ = "aircraft"
-    registration = Column(String(255), primary_key=True)
-    manufacturericao = Column(String(255), nullable=False)
-    model = Column(String(255), nullable=False)
-    typecode = Column(String(255), nullable=False)
-    viable = Column(Boolean, nullable=False, default=True)
+
+    registration: Mapped[str] = mapped_column(primary_key=True)
+    manufacturericao: Mapped[str] = mapped_column(nullable=False)
+    model: Mapped[str] = mapped_column(nullable=False)
+    typecode: Mapped[str] = mapped_column(ForeignKey('planetypes.model'), nullable=False)
+    viable: Mapped[bool] = mapped_column(nullable=False, default=True)
 
 
 class PlaneType(Base):
     __tablename__ = "planetypes"
-    model = Column(String(255), primary_key=True)
-    num_planes = Column(Integer, nullable=False)
-    weight = Column(Float, nullable=True)
+
+    model: Mapped[str] = mapped_column(primary_key=True)
+    num_planes: Mapped[int] = mapped_column(nullable=False)
+    weight: Mapped[float] = mapped_column(nullable=True)
 
 
 class Quote(Base):
     __tablename__ = "quotes"
-    index = Column(Integer, primary_key=True)
-    quote = Column(String(255), nullable=False)
-    author = Column(String(255), nullable=False)
+
+    index: Mapped[int] = mapped_column(primary_key=True)
+    quote: Mapped[str] = mapped_column(nullable=False)
+    author: Mapped[str] = mapped_column(nullable=False)
     
